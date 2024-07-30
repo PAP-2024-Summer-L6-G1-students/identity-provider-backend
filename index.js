c968bb11b331c73c88f5d14894d75ab8d23944e8
chwrd <hocheng06@gmail.com>
2024-07-30 15:28:35 -0700

    added user schema

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const cors = require('cors');
 const express = require('express');
 const { connectMongoose } = require('./connect');
+const Users = require('./models/Users');
 
 const app = express();
 const port = process.env.PORT || 3002;
 
 app.use(cors());
 app.use(express.json());
+
+
+app.get('/', async (req, res) => {
+
+  const results = await Users.readAll();
+  res.send(results);
+console.log(results)
+  console.log("GET request received on home page");
+});
 
 //* ********************* Launching the server **************** */
 
